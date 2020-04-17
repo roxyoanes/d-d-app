7d44cd7025c9b6a946142909b4259df88dbae12f
roxyoanes <roxioanes11@gmail.com>
2020-04-17 11:49:16 +0200

    Add nocors

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import {dndApi} from "./api";
 import './App.css';
 
 const App = () => {
   const [toggle, setToggle] = React.useState(false);
+  const [result, setResult] = React.useState([]);
+  const [error, setError] = React.useState(null);
 
-  const handleClick = () => {
+  const handleClick = (a) => {
+    dndApi(a).then((data) => {
+      setResult(data.results)
+      console.log(data.results);
+    })
     setToggle(!toggle);
     console.log("clicked");
   }
  
+  if (error) {
+    return <p>{error}</p>;
+  }
   return (
     <div className="App">
         <p>
           hello
         </p>
-        <button onClick={handleClick}>Classes</button>
+        <button onClick={() => handleClick("classes")}>classes</button>
         <button onClick={handleClick}>Features</button>
         <button onClick={handleClick}>Monsters</button>
         <button onClick={handleClick}>Specs</button>
--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 export const dndApi = async (category) => {
   try{
+    let nocors= `https://cors-anywhere.herokuapp.com/`
     const response = await fetch (
-      `http://dnd5eapi.co/api/${category}/`
+      `${nocors}http://dnd5eapi.co/api/${category}`
     );
     const data = await response.json();
     return data;
