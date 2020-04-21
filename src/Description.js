d02be1fc13b831b1e3fdde4eef284ca36e94e6eb
roxyoanes <roxioanes11@gmail.com>
2020-04-21 10:04:02 +0200

    Add name and description

--- src/Description.js
+++ src/Description.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-const Description = ({handleSwitch, description}) => {
-  return(
-    <div>
-      <p>Description</p>
-      <button onClick={handleSwitch}>switch</button>
-    </div>
-
-    
-  )
-}
+const Description = ({ handleSwitch, description }) => {
+    return (
+      <div>
+        <p>Description</p>
+        <button onClick={handleSwitch}>switch</button>
+        <h4>{description.name}</h4>
+        <p>{description.desc}</p>
+       </div>
+    );
+  
+};
 
 export default Description;
